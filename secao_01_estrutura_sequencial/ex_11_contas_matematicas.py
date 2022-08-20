5a3c9168012d71692570ff0269f1ed1b1f8b283f
francelmo <francelmo87@gmail.com>
2022-08-20 19:28:41 -0300

    Exercicio 11 da estrura sequencial resolvida - contas_matematicas

--- secao_01_estrutura_sequencial/ex_11_contas_matematicas.py
+++ secao_01_estrutura_sequencial/ex_11_contas_matematicas.py
@@ -23,4 +23,14 @@ Faça um Programa que peça 2 números inteiros e um número real. Calcule e mos
 def calcular_formulas():
     """Escreva aqui em baixo a sua solução"""
 
+    num1 = int(input('Digite um número inteiro:'))
+    num2 = int(input('Digite um outro numero inteiro: '))
+    num3 = float(input('Digite um número real: '))
+    a = (2 * num1) * (num2 / 2)
+    b = (3 * num1) + num3
+    c = num3 ** 3
+    print(f'O produto do dobro do primeiro com metade do segundo é {a:.2f}')
+    print(f'A soma do triplo do primeiro com o terceiro é {b}')
+    print(f'O terceiro elevado ao cubo é {c:.2f}')
+
 
