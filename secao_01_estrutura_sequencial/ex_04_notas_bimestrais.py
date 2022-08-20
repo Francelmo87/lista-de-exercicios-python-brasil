02f0ce344e7d46f08c18bb4a71e7ceebf5b4b634
francelmo <francelmo87@gmail.com>
2022-08-20 18:15:51 -0300

    Exercicio 04 da estrura sequencial resolvida - notas_bimestrais

--- secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
+++ secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
@@ -16,3 +16,10 @@ Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 def calcular_media():
     """Escreva aqui em baixo a sua solução"""
 
+    nota1 = float(input('Digite a nota do 1º bimestre: '))
+    nota2 = float(input('Digite a nota do 2º bimestre: '))
+    nota3 = float(input('Digite a nota do 3º bimestre: '))
+    nota4 = float(input('Digite a nota do 4º bimestre: '))
+    media = (nota1 + nota2 + nota3 + nota4) / 4
+    print(f'A média do Aluno foi: {media}')
+
