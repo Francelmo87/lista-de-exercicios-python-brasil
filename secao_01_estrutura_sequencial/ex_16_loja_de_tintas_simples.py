65999a9dd73c202e6a5ea8dcc643c07713890cf9
francelmo <francelmo87@gmail.com>
2022-08-20 20:10:28 -0300

    Exercicio 16 da estrura sequencial resolvida - loja_de_tintas_simples

--- secao_01_estrutura_sequencial/ex_16_loja_de_tintas_simples.py
+++ secao_01_estrutura_sequencial/ex_16_loja_de_tintas_simples.py
@@ -21,3 +21,14 @@ Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e 
 
 def calcular_latas_e_preco_de_tinta():
     """Escreva aqui em baixo a sua solução"""
+
+    from math import ceil
+
+    area = float(input('Digite a área em m² a ser Pintada: '))
+    litros_por_m2 = 3
+    litros_a_serem_usados = area / litros_por_m2
+    litros_por_latas = 18
+    numero_de_latas = ceil(litros_a_serem_usados / litros_por_latas)
+    valor_da_lata = 80
+    valor_total = numero_de_latas * valor_da_lata
+    print(f'Você deve comprar {numero_de_latas} lata(s) tinta ao custo de R$ {valor_total:.2f}')
