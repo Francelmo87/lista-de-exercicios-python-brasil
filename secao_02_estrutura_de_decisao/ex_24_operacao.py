5f7166a1ef1e11f5c4a6e8c84a5ed147c0a924f7
franc <francelmo87@hotmail.com>
2022-10-24 17:49:15 -0300

    Exercício 24 da estrutura de decisão resolvida - operaçao

--- secao_02_estrutura_de_decisao/ex_24_operacao.py
+++ secao_02_estrutura_de_decisao/ex_24_operacao.py
@@ -31,3 +31,39 @@ Mostre o restultado com duas casas decimais
 
 def fazer_operacao_e_classificar(n_1: float, n_2: float, operacao: str):
     """Escreva aqui em baixo a sua solução"""
+    if operacao == "+":
+        resultado = n_1 + n_2
+        print(f'Resultado: {resultado:.2f}')
+    elif operacao == "-":
+        resultado = n_1 - n_2
+        print(f'Resultado: {resultado:.2f}')
+    elif operacao == "*":
+        resultado = n_1 * n_2
+        print(f'Resultado: {resultado:.2f}')
+    elif operacao == "/":
+        resultado = n_1 / n_2
+        print(f'Resultado: {resultado:.2f}')
+
+    paridade_str = positividade_str = inteireza_str = ''
+
+    if resultado % 2 == 0:
+        paridade_str = 'par'
+    else:
+        paridade_str = 'impar'
+
+    if resultado == 0:
+        positividade_str = 'neutro'
+    elif resultado > 0:
+        positividade_str = 'positivo'
+    elif resultado < 0:
+        positividade_str = 'negativo'
+
+    if resultado // 1 == resultado:
+        inteireza_str = 'inteiro'
+    else:
+        inteireza_str = 'decimal'
+
+    if n_1 // 1 == n_1:
+        print(f'Número é {paridade_str}, {positividade_str} e {inteireza_str}.')
+    else:
+        print(f'Número é {positividade_str} e {inteireza_str}.')
--- secao_02_estrutura_de_decisao/ex_26_posto_de_gasolina.py
+++ secao_02_estrutura_de_decisao/ex_26_posto_de_gasolina.py
@@ -35,3 +35,12 @@ Mostre o restultado com duas casas decimais
 
 def calcular_abastecimento(litros_de_combustivel: float, tipo_de_combustivel: str) -> str:
     """Escreva aqui em baixo a sua solução"""
+    litros_de_combustivel = 10
+    tipo_de_combustivel = 'A'
+    preco_da_gasolina = 2.5
+    preço_do_alcool = 1.9
+
+    litros_de_combustivel = tipo_de_combustivel = preço_do_alcool = preco_da_gasolina = ''
+
+    if litros_de_combustivel <= 20:
+        preço_do_alcool - (preço_do_alcool)
